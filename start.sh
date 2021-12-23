597024ea3e8239e7698ec583e382e3da49ff1224
Ben Chatelain <ben@octop.ad>
2021-12-22 20:21:11 -0700

    🥅 Handle missing dir by exiting with error

--- start.sh
+++ start.sh
@@ -5,10 +5,9 @@
 #
 # https://gregmfoster.medium.com/using-m1-mac-minis-to-power-our-github-actions-ios-ci-540c55af13ea
 
-cd actions-runner
+cd actions-runner || exit 1
 
 # Only needs to be called once
 #arch -x86_64 ./config.sh --url https://github.com/mas-cli/mas --token ******
 
 arch -x86_64 ./run.sh
-
