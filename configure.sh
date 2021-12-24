cea81766d6e82f99d7affa70616fc38572ffc0d4
Ben Chatelain <ben@octop.ad>
2021-12-24 16:11:07 -0700

    🔨 Read GitHub TOKEN from environment

--- configure.sh
+++ configure.sh
@@ -6,6 +6,12 @@
 # Only needs to be called once
 # https://github.com/actions/runner/blob/801a02ec891bb7aac65addc49eb6eeaeb942cf3e/src/Runner.Common/Constants.cs#L90
 
+
+if test -z "${TOKEN}"; then
+    echo "Required TOKEN environment variable is not set"
+    exit 1
+fi
+
 cd actions-runner
 
 # Organization level (haven't gotten this to work)
@@ -13,13 +19,13 @@ cd actions-runner
 # token=******
 
 # Repository level
-url=https://github.com/mas-cli/mas
-token=******
+url="https://github.com/mas-cli/mas"
+token="${TOKEN}"
 
 ./config.sh \
     --name "m1" \
     --runnergroup "Default" \
-    --labels "self-hosted,macOS,arm64" \
+    --labels "self-hosted,macOS" \
     --work "_work" \
     --url "${url}" \
     --token "${token}"
